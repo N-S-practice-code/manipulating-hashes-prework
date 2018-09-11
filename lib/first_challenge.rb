6db6769c45b66faa53f68d9373013657c1f06701
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-11 14:34:45 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
   contacts.each do |person, data|
-  puts "#{person}: #{data}"
+      data.each do |attribute, value|
   end
   
   #remember to return your newly altered contacts hash!
